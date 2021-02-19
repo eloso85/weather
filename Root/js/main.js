4187b66bb28283c116a229c861ab73d53295580e
Alejandro <eloso85@gmail.com>
2021-02-18 23:36:14 -0600

    made card and icon

--- Root/js/main.js
+++ Root/js/main.js
@@ -1,9 +1,9 @@
-var DateTime = luxon.DateTime
 
 
 
-var dataDate = new Date(1613066400 * 1000)
-console.log(dataDate.toDateString())
+// var DateTime = luxon.DateTime
+// var dataDate = new Date(1613066400 * 1000)
+// console.log(dataDate.toDateString())
 
 
 let luxDate = (input) => {
@@ -28,10 +28,18 @@ function successCallback(data){
           .then(response => response.json())
           .then(locData=>{
             console.log(locData)
-            $(".today").html(`<div>${locData.name}${Math.round(locData.main.temp)}</div>`)
-          })
-}
-
+            
+            $(".today").html(`<div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="http://openweathermap.org/img/wn/${locData.weather[0].icon}@2x.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Today's Tempeture</h5>
+              <p class="card-text">${locData.name} ${Math.round(locData.main.temp)} </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>`)
+          });
+};
+//<div>${locData.name}${Math.round(locData.main.temp)}</div>
 
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=London,us&units=imperial&APPID=166a433c57516f51dfab1f7edaed8413")
@@ -76,58 +84,3 @@ $("#submit").on("click", () => {
 
 
 
-// $("#submit").on("click", function () {
-//     /*var APIKey = "166a433c57516f51dfab1f7edaed8413";*/
-//     var city = $("#search").val();
-//     // Here we are building the URL we need to query the database
-//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + ",&cnt=7&us&units=imperial&APPID=166a433c57516f51dfab1f7edaed8413";
-//     var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&APPID=166a433c57516f51dfab1f7edaed8413";
-//     // Here we run our AJAX call to the OpenWeatherMap API
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     })
-//       // We store all of the retrieved data inside of an object called "response"
-//       .then(function (response) {
-
-//           console.log(response);
-
-
-//         $(".container").html(`${response.list.map(function(days){
-//           console.log(days)
-//            var dataDate = luxDate(days.dt)
-//           console.log(dataDate)
-//           return`
-//           <div class="card text-center" style="width: 25rem;">
-//               <div class="card-body">
-//                 <h5>${Math.round(days.temp.max) + "/" + Math.round(days.temp.min)}</h5>
-//                   <h5 class="card-title-0">${dataDate.toDateString()}</h5>
-//                     </div>
-//                         </div>`
-
-
-
-//         }).join("")}`);
-
-//         });
-//       event.preventDefault()
-//   });
-//   ;
-  // $("#submit").on("click", function () {
-  //   /*var APIKey = "166a433c57516f51dfab1f7edaed8413";*/
-  //   var city = $("#search").val();
-  //   // Here we are building the URL we need to query the database
-
-  //   var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&APPID=166a433c57516f51dfab1f7edaed8413";
-  //   // Here we run our AJAX call to the OpenWeatherMap API
-  //   $.ajax({
-  //     url: queryURL2,
-  //     method: "GET"
-  //   })
-
-  //     .then(function (response2) {
-  //       console.log(response2.name)
-  //       //console.log(queryURL2)
-  //       $(".wind").html("<h1>" + response2.name + " Current Weather Details </h1>"); 
-  //     })
-  // })
